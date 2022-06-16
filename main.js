ab96aaa93b447f4a4e8c0fea07e7b0a83e80c892
mamo2003 <mlarraniaga1978@gmail.com>
2022-06-15 22:14:19 -0300

    primeros ejercicos

---
+++ main.js
@@ -0,0 +1,54 @@
+// console.log("hola mundo");
+// let apellido='larrañaga';
+// let edad= 44;
+// console.log(edad+1);
+// console.log(apellido+1);
+// console.log(apellido+' '+1);
+// console.log(10>11);
+// console.log(11>=10);
+// console.log(10===12);
+// console.log(10!==11);
+// console.log(apellido-1);
+// const numeropi = 6.14;
+// let rol;
+// console.log(rol);
+// alert("bienvenido a esta pagina");
+// let nombreingresado = prompt("ingrese su nombre");
+// let edadingresada= prompt("ingrese su edad");
+// let edadennumero= parseInt(edadingresada);
+// alert("hola"+" "+nombreingresado);
+// alert("usted cumplira"+(edadennumero+1)+" "+ "en su proximo cumpleaños.");
+// if(){}
+
+
+// // ejercicio 1
+// alert("bienvenido!!")
+// let unnumero = prompt("elige un numero?");
+// let primernumero=parseInt(unnumero);
+// let dnumero = prompt ("elige otro numero?");
+// let segundonumero = parseInt(dnumero)
+// if(primernumero>segundonumero){console.log(primernumero + segundonumero);+console.log(primernumero - segundonumero)}
+// if(primernumero<segundonumero){console.log(primernumero*segundonumero);+console.log(primernumero/segundonumero)}
+
+// ejercicio2
+alert("analicemos tus notas !!")
+let p_nota = prompt("que nota sacaste en matematica?");
+let primernota=parseInt(p_nota);
+let snota = prompt ("que nota sacaste en quimica?");
+let segundanota = parseInt(snota);
+let tnota = prompt("que nota sacaste en historia?");
+let tercernota=parseInt(tnota);
+let totalnotas=(primernota+segundanota+tercernota);
+if(primernota>10 || segundanota>10 || tercernota>10){console.log("alguna nota no es correcta.. revisa y vuelve a  ingresar")}
+if(totalnotas/3 >=7 && totalnotas<=30){console.log("has promocionado la cursada, felicitaciones!")}
+if(totalnotas/3 <7){console.log("nos vemos en diciembre para seguir repasando")}
+
+
+// // ejercico 3
+// let pnumero = prompt ("elige un numero del 1 al 99");
+// let numero =parseInt(pnumero);
+// if(numero>=1 && numero<=9){console.log("tiene un solo digito")}
+// if(numero>=10 && numero<=99){console.log("posee dos digitos")}
+// if(numero>99){console.log("el numero ingresado no cumple con la condicion solicitada! prueba nuevamente!")}
+
+
