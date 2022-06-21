39311a1c5a63983baecb01393d9e7d2f400b8908
mamo2003 <mlarraniaga1978@gmail.com>
2022-06-21 17:39:21 -0300

    correccion else if

--- main.js
+++ main.js
@@ -31,7 +31,7 @@
 // if(primernumero<segundonumero){console.log(primernumero*segundonumero);+console.log(primernumero/segundonumero)}
 
 // ejercicio2
-alert("analicemos tus notas !!")
+/* alert("analicemos tus notas !!")
 let p_nota = prompt("que nota sacaste en matematica?");
 let primernota=parseInt(p_nota);
 let snota = prompt ("que nota sacaste en quimica?");
@@ -40,15 +40,14 @@ let tnota = prompt("que nota sacaste en historia?");
 let tercernota=parseInt(tnota);
 let totalnotas=(primernota+segundanota+tercernota);
 if(primernota>10 || segundanota>10 || tercernota>10){console.log("alguna nota no es correcta.. revisa y vuelve a  ingresar")}
-if(totalnotas/3 >=7 && totalnotas<=30){console.log("has promocionado la cursada, felicitaciones!")}
-if(totalnotas/3 <7){console.log("nos vemos en diciembre para seguir repasando")}
+else if(totalnotas/3 >=7 && totalnotas<=30){console.log("has promocionado la cursada, felicitaciones!")}
+else{console.log("nos vemos en diciembre para seguir repasando")} */
 
-
-// // ejercico 3
+// ejercico 3
 // let pnumero = prompt ("elige un numero del 1 al 99");
 // let numero =parseInt(pnumero);
 // if(numero>=1 && numero<=9){console.log("tiene un solo digito")}
-// if(numero>=10 && numero<=99){console.log("posee dos digitos")}
-// if(numero>99){console.log("el numero ingresado no cumple con la condicion solicitada! prueba nuevamente!")}
+// else if(numero>=10 && numero<=99){console.log("posee dos digitos")}
+// else{console.log("el numero ingresado no cumple con la condicion solicitada! prueba nuevamente!")} */
 
 
