071ba3877b3ff50edd44cc27ed7742d70b78b907
mamo2003 <mlarraniaga1978@gmail.com>
2022-07-03 11:32:35 -0300

    objetos literales

---
+++ objetos/mainob.js
@@ -0,0 +1,22 @@
+
+let persona1 = {
+    nombre: "mathias",
+    edad: 29,
+    profesor: true,
+}
+
+let persona2 ={
+    nombre: "juan",
+    edad: 35,
+    profesor: false,
+}
+
+let persona3 ={
+    nombre: "felicitas",
+    edad: 18,
+    profesor: true,
+}
+
+const PERSONAL= [persona1, persona2, persona3]
+// console.log(PERSONAL);
+// console.table(PERSONAL);
